2c780149b10bee49abb55d4f86f8278a42dd1ae3
Nicolas Escobar <nescoba@iu.edu>
2024-08-27 10:12:43 -0400

    Adding URL

--- R/environments.R
+++ R/environments.R
@@ -3,9 +3,9 @@ il <- function(x) paste0("$", x, "$")
 lenv <- function(name, rows){
 	   paste0("\\begin{",
 		  name,
-		  "}\\n",
-		  Reduce(\(x,y) paste0(x,"\\n",y), rows),
-		  "\\n\\end{",
+		  "}",
+		  Reduce(\(x,y) paste0(x,y), rows),
+		  "\\end{",
 		  name,
 		  "}"
 	   )
